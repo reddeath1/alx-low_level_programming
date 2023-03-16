8377f05d8b55f4bfecb972af5186ab949b8e7904
reddeath1 <frankslayer1@gmail.com>
2023-03-16 16:50:27 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,7 @@
 
 int main(void)
 {
-	int len = strlen("and that piece of art is useful\" 
-			- Dora Korpar, 2015-10-19\n");
-	write(STDOUT_FILENO,"and that piece of art is useful\"
-		       	- Dora Korpar, 2015-10-19\n",len);
+	int len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",len);
 	return (1);
 }
