c98d2addd1179c01bff35f080c9c3d4e7b1b98bb
reddeath1 <frankslayer1@gmail.com>
2023-03-17 16:24:56 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main bloc
+ *
+ * Return: always
+ */
+
+int main(void)
+{
+	char chars;
+
+	for (chars = 'z'; chars <= 'a'; chars++)
+		putchar(chars);
+	putchar('\n');
+	return (0);
+}
