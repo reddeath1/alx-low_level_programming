a086bbd1e18952b6c049d55501b5060b914c6eaf
reddeath1 <frankslayer1@gmail.com>
2023-03-17 16:45:15 -0700

    reverse alphabet

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,12 @@ int main(void)
 {
 	int chars;
 
-	for (chars = 0; chars <= 16; chars++)
-		putchar(chars + '0');
+	for (chars = 0; chars < 16; chars++)
+		if (chars < 10)
+			putchar(chars + '0');
+		else
+			putchar('a' + (chars - 10));
+
 	putchar('\n');
 	return (0);
 }
