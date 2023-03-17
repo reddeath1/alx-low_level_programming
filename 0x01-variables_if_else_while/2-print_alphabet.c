17fb5a3852dd50115689bd4b6e95c295ad03313e
reddeath1 <frankslayer1@gmail.com>
2023-03-17 14:40:40 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - main bloc
+ *
+ * Return: always
+ */
+
+int main(void)
+{
+
+	char chars = 'a';
+
+	while (chars <= 'z')
+	{
+		putchar(chars);
+		chars++;
+	}
+
+	return (0);
+}
