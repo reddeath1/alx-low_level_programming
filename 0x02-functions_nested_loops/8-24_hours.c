d2287783caac7ae8f993cdf290411af15219abf4
reddeath1 <frankslayer1@gmail.com>
2023-03-21 16:03:12 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *
- * Return: always ()
+ * jack_baer - main body
+ * Description: produce hrs and minute of a day
  */
 
 void jack_bauer(void)
@@ -10,7 +10,6 @@ void jack_bauer(void)
 	int hour, minute;
 
 	for (hour = 0; hour < 24; hour++)
-	{
 		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar((hour / 10) + '0');
@@ -20,5 +19,4 @@ void jack_bauer(void)
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
-	}
 }
