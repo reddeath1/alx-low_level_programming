e6df9cf10ae6f5ed87c1ce7c73862239d4ed3a2f
reddeath1 <frankslayer1@gmail.com>
2023-03-21 14:30:32 -0700

    alphabet to 10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - body
+ * return: always 
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - body
+ * Decription: function main objective  is to print alphabet
+ * return: always
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	for (; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
