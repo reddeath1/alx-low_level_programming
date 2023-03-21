2f3693eac9f336318abd21260cb6e63a63c10ad5
reddeath1 <frankslayer1@gmail.com>
2023-03-21 16:39:45 -0700

    count

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - main body
+ * @n: input
+ * Description: count fron n to 98
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
