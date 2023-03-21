78338b48505826368b6c70ec78a96658f87cf004
reddeath1 <frankslayer1@gmail.com>
2023-03-21 13:11:33 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - body
+ * decription: function
+ * return
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while ( c < 'z')
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+
+	return (0);
+}
