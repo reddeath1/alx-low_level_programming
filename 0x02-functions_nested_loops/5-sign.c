d40a80990dc462aa28e54694c0907d5262bfc751
reddeath1 <frankslayer1@gmail.com>
2023-03-21 15:21:38 -0700

    if else

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,19 +6,21 @@
 int print_sign(int n)
 {
 	int nu;
-
+	
 	if (n > 0)
+	{
 		nu = 1;
 		_putchar('+');
+	}
 	else if (n == 0)
 	{
 		nu = 0;
-		_putchar('-');
+		_putchar('0');
 	}
 	else
 	{
 		nu = -1;
-		_putchar("-");
+		_putchar('-');
 	}
 	return (nu);
 }
