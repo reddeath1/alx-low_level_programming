46e89542ef2efa94890f58e9022e9d4dd187fcf1
reddeath1 <frankslayer1@gmail.com>
2023-03-21 12:30:43 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,12 +13,12 @@ int main(void)
 	char str[8] =  "_putchar";
 	int i = 0;
 
-	while(i <= 8){
+	while (i <= 8){
 		_putchar(str[i]);
 		i++;
 	}
 
-	_putchar('\n');	
+	_putchar('\n');
 
 	return (0);
 }
