f9d7c0f70a71fb601b1698466e05473991813b9d
reddeath1 <frankslayer1@gmail.com>
2023-03-21 15:01:04 -0700

    is lower or upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Check
+ * @i: input 
+ * Description: function returns 1 if the character is a
+ * character, lowercase or uppercase.
+ * Return: 1 or 0.
+ */
+int _isalpha(int i)
+{
+	char islower, isupper;
+	int ischar = 0;
+
+	for (islower = 'a'; islower <= 'z'; islower++)
+		for (isupper = 'A'; isupper <= 'Z'; isupper++)
+			if (c == islower || c == isupper)
+				ischar = 1;
+	return (ischar);
+}
