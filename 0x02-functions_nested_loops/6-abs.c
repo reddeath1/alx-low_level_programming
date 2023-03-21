12c6a1c9cd53e9bc9db20474c0fa6118e9e2b214
reddeath1 <frankslayer1@gmail.com>
2023-03-21 15:35:41 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _abs
- *
+ * _abs - main body
+ * @v: input
  * Return: alwasy ()
  */
 
 
 int _abs(int v)
 {
-	if(v > 0)
+	if (v > 0)
 		return (v);
 	else
 		return (v * -1);
