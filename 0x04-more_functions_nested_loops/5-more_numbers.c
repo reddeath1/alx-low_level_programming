c707557427b490e82c3f967883c254dee4ef6311
reddeath1 <frankslayer1@gmail.com>
2023-03-23 14:14:56 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print function
+ * Decription:
+ */
+
+void more_numbers(void)
+{
+
+	int i = 0, j;
+
+	while (i <= 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+}
