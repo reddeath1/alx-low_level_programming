ed262173c40ab30e03bc9ab865f17e6e9d5ea7d4
reddeath1 <frankslayer1@gmail.com>
2023-03-23 14:36:44 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - print function
+ * Decription:
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
