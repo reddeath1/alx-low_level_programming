30d67d2490adbc3821b930e93879d68135579385
reddeath1 <frankslayer1@gmail.com>
2023-03-23 13:48:19 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print function
+ * Decription:
+ */
+
+void print_numbers(void)
+{
+
+	int i = '0';
+
+	while(i <= '9')
+	{
+		_putchar(i);
+		_putchar('\n');
+		i++;
+	}
+}
