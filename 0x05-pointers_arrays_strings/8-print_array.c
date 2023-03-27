63a1cc29804a1c302a5f5dac7c7fd5a662368f27
reddeath1 <frankslayer1@gmail.com>
2023-03-27 16:17:54 -0700

    print arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - main func
+ * @a: arg
+ * @b: arg2
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
