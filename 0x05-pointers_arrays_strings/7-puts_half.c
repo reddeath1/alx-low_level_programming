e2fe74a69e2127b3533853ae1a480ff86220d9aa
reddeath1 <frankslayer1@gmail.com>
2023-03-27 16:02:54 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - main func
+ * @str: arg
+ * Description: Print 1st char, and even
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
