ad64130a5fe100204f657121826c7842c5df294d
reddeath1 <frankslayer1@gmail.com>
2023-03-30 09:45:46 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - main func
+ * @dest: arg
+ * @src: arg2
+ * Return: always
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+
+	/* Finding the end of dest */
+	while (*i != '\0')
+		i++;
+
+	/* Append src to dest */
+	while (*src != '\0')
+	{
+		*i = *src;
+		i++;
+		src++;
+	}
+
+	/* Add terminating null byte */
+	*i = '\0';
+
+	return dest;
+}
+
