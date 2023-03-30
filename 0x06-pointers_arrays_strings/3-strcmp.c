2e6f4fc84d18c4c18f6681177ae58cf98b25e529
reddeath1 <frankslayer1@gmail.com>
2023-03-30 14:15:46 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - main func
+ * #s1: arg1
+ * @s2: arg
+ * Return: always
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
