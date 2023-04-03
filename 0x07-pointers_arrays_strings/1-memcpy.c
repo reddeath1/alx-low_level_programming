fb78203928c2d03755fe06b4bfd2d5d5951bb5d4
reddeath1 <frankslayer1@gmail.com>
2023-04-03 15:01:06 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - main func
+ * @dest: arg
+ * @src: arg1
+ * @n: arg2
+ * Return: always
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest = *src;
+	}
+
+
+	return (dest);
+}
