085ceda2f90a6d37cefc3a21ccdd1ac2bb7e5599
Reddeath <frankslayer1@gmail.com>
2023-10-12 01:31:03 +0300

    search algorithm

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - The main function performs exponential search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 1, siz = 0;
+	int ret;
+
+	if (!array || !size)
+		return (-1);
+
+	while (i < size && array[i] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i <<= 1;
+	}
+	siz = (i >= size ? size : i + 1) - (i >> 1);
+	i >>= 1;
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			i, i << 1 >= size ? size - 1 : i << 1);
+	ret = binary_search(array + i, siz, value);
+	return (ret == -1 ? ret : ret + (int)i);
+}
+
+
